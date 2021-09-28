c4205f07f29e19326fdfa0ed41155844f066477a
EC2 Default User <ec2-user@ip-172-31-27-165.ec2.internal>
2021-09-28 05:15:00 +0000

    [finish]2-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '99783b590120fa88247ce1fd7656551bbfeb85b373c37805d773e239e6630be2d7625561fab2c87732cfebf76182a4d32ef9763d1e7772dd91fbc0be0612a7d4'
---
+++ db/migrate/20210928035029_ass_image_id_to_list.rb
@@ -0,0 +1,4 @@
+class AssImageIdToList < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
